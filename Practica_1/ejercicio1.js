c57f2b231770352535b1d76090ae6863402fb18f
Darko1901 <123046770@upq.edu.mx>
2026-06-01 14:54:14 -0600

    Primer ejercicio de estudio para la práctica 1.

---
+++ Practica_1/ejercicio1.js
@@ -0,0 +1,11 @@
+var nombre = "Armando";
+const edad = 25;
+
+nombre = "Ana María";
+
+var saludo = "Hola, " + nombre + " .Tienes " + edad + " años.";
+
+// mostrar en consola el saludo 
+console.log(saludo);
+
+
