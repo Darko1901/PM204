cf3acd0ec048602fa1eddb16f9a5b697256e8e35
Darko1901 <123046770@upq.edu.mx>
2026-06-01 14:58:44 -0600

    Ejercicio 2 realizado para la práctica 1.

---
+++ Practica_1/ejercicio2.js
@@ -0,0 +1,14 @@
+// funcion original
+function cuadrado(numero){
+    return numero * numero;
+}
+
+// funcion flecha
+const cuadradoff = numero => numero * numero;
+
+console.log(cuadradoff(2));
+console.log(cuadradoff(3));
+console.log(cuadradoff(5));
+
+
+
